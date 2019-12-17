d1083f60736f60f93bcd55b2da7774ed4464cf56
Matt Precious <mattp@squareup.com>
2019-12-17 16:56:32 -0500

    Two space indent for build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,62 +1,62 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    java
-    kotlin("jvm") version "1.3.61"
+  java
+  kotlin("jvm") version "1.3.61"
 }
 
 group = "com.mattprecious"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    implementation("com.jakewharton.picnic", "picnic", "0.2.0")
-    implementation("com.squareup.okio", "okio", "2.4.2")
-    implementation("com.squareup.wire", "wire-runtime", "3.0.2")
+  implementation(kotlin("stdlib-jdk8"))
+  implementation("com.jakewharton.picnic", "picnic", "0.2.0")
+  implementation("com.squareup.okio", "okio", "2.4.2")
+  implementation("com.squareup.wire", "wire-runtime", "3.0.2")
 
-    testCompile("junit", "junit", "4.12")
+  testCompile("junit", "junit", "4.12")
 }
 
 configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
+  sourceCompatibility = JavaVersion.VERSION_1_8
 }
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+  kotlinOptions.jvmTarget = "1.8"
 }
 
 val fatJar = task("fatJar", type = Jar::class) {
-    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
-    with(tasks.jar.get() as CopySpec)
+  from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+  with(tasks.jar.get() as CopySpec)
 
-    archiveClassifier.set("fat")
+  archiveClassifier.set("fat")
 
-    manifest {
-        attributes["Main-Class"] = "com.mattprecious.protoprinter.ProtoPrinterKt"
-    }
+  manifest {
+    attributes["Main-Class"] = "com.mattprecious.protoprinter.ProtoPrinterKt"
+  }
 }
 
 val binaryJar = task("binaryJar") {
-    val binaryFile = File(buildDir, "proto-printer")
+  val binaryFile = File(buildDir, "proto-printer")
 
-    doLast {
-        val fatJarFile = fatJar.archiveFile.get().asFile
+  doLast {
+    val fatJarFile = fatJar.archiveFile.get().asFile
 
-        binaryFile.parentFile.mkdirs()
-        binaryFile.appendText("#!/bin/sh\n\nexec java -jar \$0 \"\$@\"\n\n")
-        binaryFile.appendBytes(fatJarFile.readBytes())
+    binaryFile.parentFile.mkdirs()
+    binaryFile.appendText("#!/bin/sh\n\nexec java -jar \$0 \"\$@\"\n\n")
+    binaryFile.appendBytes(fatJarFile.readBytes())
 
-        binaryFile.setExecutable(true)
-    }
+    binaryFile.setExecutable(true)
+  }
 }
 
 tasks {
-    "assemble" {
-        dependsOn(binaryJar)
-    }
+  "assemble" {
+    dependsOn(binaryJar)
+  }
 
-    binaryJar.dependsOn(fatJar)
+  binaryJar.dependsOn(fatJar)
 }
