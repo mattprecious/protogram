e5385b268b184eb7787adf7ae4dc88c57b7bd178
Jake Wharton <jakew@google.com>
2019-12-19 15:49:40 -0500

    Add clikt since more options will be coming

--- cli/src/main/kotlin/com/mattprecious/protogram/main.kt
+++ cli/src/main/kotlin/com/mattprecious/protogram/main.kt
@@ -2,10 +2,24 @@
 
 package com.mattprecious.protogram
 
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.convert
 import okio.ByteString.Companion.decodeHex
+import java.io.PrintStream
 
 fun main(vararg args: String) {
-  require(args.size == 1) { "Only one arg supported" }
-  val bytes = args.single().decodeHex()
-  println(printProto(bytes))
+  ProtogramCli(System.out).main(args.toList())
+}
+
+private class ProtogramCli(
+  private val out: PrintStream
+) : CliktCommand(name = "protogram") {
+
+  private val bytes by argument("hex", "Encoded proto bytes as hex")
+      .convert { it.decodeHex() }
+
+  override fun run() {
+    out.println(printProto(bytes))
+  }
 }
