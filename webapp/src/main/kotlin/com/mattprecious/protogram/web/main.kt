d286b2d569705bcf34e4ffc451573760c542770b
Jake Wharton <jakew@google.com>
2019-12-19 22:19:41 -0500

    Make webapp reactive

--- webapp/src/main/kotlin/com/mattprecious/protogram/web/main.kt
+++ webapp/src/main/kotlin/com/mattprecious/protogram/web/main.kt
@@ -1,49 +1,77 @@
 package com.mattprecious.protogram.web
 
 import com.mattprecious.protogram.printProto
+import kotlinx.coroutines.await
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import okio.Buffer
 import okio.ByteString
 import okio.ByteString.Companion.decodeHex
 import org.khronos.webgl.ArrayBuffer
 import org.khronos.webgl.Uint8Array
 import org.khronos.webgl.get
-import org.w3c.dom.HTMLButtonElement
 import org.w3c.dom.HTMLInputElement
 import org.w3c.dom.HTMLTextAreaElement
+import org.w3c.dom.events.Event
+import org.w3c.dom.events.EventTarget
 import org.w3c.files.Blob
 import org.w3c.files.File
 import org.w3c.files.FileList
 import kotlin.browser.document
 import kotlin.js.Promise
 
-fun main() {
+suspend fun main() = coroutineScope<Unit> {
   val input = document.getElementById("input") as HTMLTextAreaElement
   val file = document.getElementById("file") as HTMLInputElement
   val output = document.getElementById("output") as HTMLTextAreaElement
-  val convert = document.getElementById("convert") as HTMLButtonElement
 
-  file.addEventListener("change", callback = {
-    val files = it.target.asDynamic().files as FileList
-    files.firstOrNull()
-        ?.arrayBuffer()
-        ?.then {
-          input.value = it.toByteString().hex()
-          convert.click()
-          file.asDynamic().value = null
+  launch {
+    file.events("change")
+        .map {
+          val inputFiles = it.target.asDynamic().files as FileList
+          inputFiles.firstOrNull()
+              ?.arrayBuffer()
+              ?.await()
+              ?.toByteString()
+              ?.hex() ?: ""
         }
-  })
+        .collect {
+          // Clear input file. We use dynamic because the Kotlin type is not nullable.
+          file.asDynamic().value = null
 
-  convert.removeAttribute("disabled")
-  convert.addEventListener("click", callback = {
-    val hex = input.value.trim()
-    val tree = try {
-      printProto(hex.decodeHex())
-    } catch (e: Exception) {
-      console.error(e)
-      "Unable to decode hex\n\nError: ${e::class.simpleName} ${e.message ?: ""}"
-    }
-    output.value = tree
-  })
+          input.value = it
+          // Setting the value programmatically does not fire the 'input' event.
+          input.dispatchEvent(Event("input"))
+        }
+  }
+
+  launch {
+    input.events("input")
+        .map { input.value.trim() }
+        .collect {
+          val tree = try {
+            printProto(it.decodeHex())
+          } catch (e: Exception) {
+            console.error(e)
+            "Unable to decode hex\n\nError: ${e::class.simpleName} ${e.message ?: ""}"
+          }
+          output.value = tree
+        }
+  }
+}
+
+private fun EventTarget.events(event: String) = callbackFlow {
+  val listener: (Event) -> Unit = {
+    offer(it)
+  }
+  addEventListener(event, listener)
+  awaitClose {
+    removeEventListener(event, listener)
+  }
 }
 
 private fun FileList.firstOrNull(): File? {
