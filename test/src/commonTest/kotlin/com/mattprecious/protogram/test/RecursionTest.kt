1abfbeb57614a0a34314a0b8021d285ad84b64b4
Matt Precious <mattp@squareup.com>
2019-12-19 20:50:28 -0500

    Make a Tree class and rename/move the node types

--- test/src/commonTest/kotlin/com/mattprecious/protogram/test/RecursionTest.kt
+++ test/src/commonTest/kotlin/com/mattprecious/protogram/test/RecursionTest.kt
@@ -1,23 +1,23 @@
 package com.mattprecious.protogram.test
 
 import com.mattprecious.tinsel.Node
-import com.mattprecious.tinsel.Node.InternalNode
-import com.mattprecious.tinsel.Node.LeafNode
+import com.mattprecious.tinsel.Node.Branch
+import com.mattprecious.tinsel.Node.Leaf
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class RecursionTest {
   @Test fun recursiveTreeBuilder() {
     var actual = buildRecursiveTree(label = "2", leafValue = "v", depth = 1)
-    var expected: Node = LeafNode("2", "v")
+    var expected: Node = Leaf("2", "v")
     assertEquals(expected, actual)
 
     actual = buildRecursiveTree("2", "v", 2)
-    expected = InternalNode("2", listOf(LeafNode("2", "v")))
+    expected = Branch("2", listOf(Leaf("2", "v")))
     assertEquals(expected, actual)
 
     actual = buildRecursiveTree(label = "2", leafValue = "v", depth = 5)
-    expected = InternalNode("2", listOf(InternalNode("2", listOf(InternalNode("2", listOf(InternalNode("2", listOf(LeafNode("2", "v")))))))))
+    expected = Branch("2", listOf(Branch("2", listOf(Branch("2", listOf(Branch("2", listOf(Leaf("2", "v")))))))))
     assertEquals(expected, actual)
   }
 }
--- tinsel/src/commonMain/kotlin/com/mattprecious/tinsel/model.kt
+++ tinsel/src/commonMain/kotlin/com/mattprecious/tinsel/model.kt
@@ -1,14 +1,16 @@
 package com.mattprecious.tinsel
 
+data class Tree(val nodes: List<Node> = emptyList())
+
 sealed class Node {
   abstract val label: String
 
-  data class InternalNode(
+  data class Branch(
     override val label: String,
     val children: List<Node>
   ) : Node()
 
-  data class LeafNode(
+  data class Leaf(
     override val label: String,
     val value: String
   ) : Node()
